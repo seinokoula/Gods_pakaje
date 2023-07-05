e39cb265d12044fed86d4fc166250842eb8e1257
seinokoula <alexandre.charles92@gmail.com>
2023-07-05 14:40:31 +0200

    :tada: working on local build

---
+++ src/Api.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Sterlingarcher\GodsPakaje;
+
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Api
+{
+    public function scrapeWebsite(): array
+    {
+        $httpClient = HttpClient::create();
+        $response = $httpClient->request('GET', 'https://goods.wtf/');
+
+        $content = $response->getContent();
+
+        $crawler = new Crawler($content);
+        $itemElements = $crawler->filter('.item');
+
+        $items = [];
+        foreach ($itemElements as $itemElement) {
+            $domCrawler = new Crawler($itemElement);
+
+            $name = $domCrawler->filter('.text-secondary')->first()->text();
+            $picture = $domCrawler->filter('img')->first()->attr('src');
+            $price = $domCrawler->filter('.text-secondary')->last()->text();
+
+            $item = [
+                'name' => $name,
+                'picture' => $picture,
+                'price' => $price,
+            ];
+
+            $items[] = $item;
+        }
+
+        return $items;
+    }
+}
